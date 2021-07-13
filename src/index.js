b02581433bd0430131acdb971227a9b3a873c54e
esteban perez <epzmox@gmail.com>
2021-07-13 11:20:31 -0500

    creando los nodos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const url = "https://platzi-avo.vercel.app/api/avo"
 
-console.log('Happy hacking :)')
+
+window 
+.fetch(url)
+
+
+.then((respuesta) => respuesta.json())
+
+
+.then((rensponseJson)=>{
+    const todosLosItems = []
+    rensponseJson.data.forEach((item) => {
+        const imagen = document.createElement('img')
+        const title = document.createElement('h2')
+        const price = document.createElement('div')
+
+        const container = document.createElement('div')
+        container.append(imagen, title, price)
+
+        todosLosItems.push(container)
+        
+    });
+
+    document.body.append(...todosLosItems)
+})
