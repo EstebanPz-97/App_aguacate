7dede8975bcc16aa4f1ee41d8f4c43a29bb4898d
esteban perez <epzmox@gmail.com>
2021-07-13 14:33:22 -0500

    estilos y intl precio

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-const url = "https://platzi-avo.vercel.app/api/avo"
+const baseUrl = "https://platzi-avo.vercel.app"
+const appNode = document.querySelector("#app")
+appNode.className = "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-4"
 
-
+const formatPrice = (price) =>{
+    const nwePrice = new window.Intl.NumberFormat('en-EN',{
+        style:'currency',
+        currency: 'USD'
+    }).format(price)
+       return nwePrice
+}
 window 
-.fetch(url)
+.fetch(`${baseUrl}/api/avo`)
 
 
 .then((respuesta) => respuesta.json())
@@ -12,15 +20,23 @@ window
     const todosLosItems = []
     rensponseJson.data.forEach((item) => {
         const imagen = document.createElement('img')
+        imagen.src = `${baseUrl}${item.image}`
         const title = document.createElement('h2')
+        title.textContent = item.name
+        title.className = 'text-5xl text-green-900' 
         const price = document.createElement('div')
+        price.textContent =formatPrice(item.price)
 
         const container = document.createElement('div')
         container.append(imagen, title, price)
+        container.className = "border-4 border-dashed border-gray-200 rounded-lg h-96 hover:bg-gray-300	 hover:shadow-lg hover:border-transparent "
+        container.style.height = "auto"
+        
+
 
         todosLosItems.push(container)
         
     });
 
-    document.body.append(...todosLosItems)
+    appNode.append(...todosLosItems)
 })
